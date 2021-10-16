9ae55f120b381763682365b9736c58c8c3ee49dc
Bryan Ong <bryanongse@gmail.com>
2021-10-17 02:42:53 +0800

    Corrected some equations and wrong calculations

--- backend/area.py
+++ backend/area.py
@@ -1,41 +1,57 @@
 import cv2
 import numpy as np
+import math
 
 #input == bitmap, depth in meters
 def areas(img,depth):
-
     def hw(img):
-        maxh = 0
+        maxw = 0
         for x in range((img.shape[0])):
             for y in range((img.shape[1])):
                 if img[x,y, 0] == 8:
                     start = y
                     break
-            for y in range(img.shape[1]-1,0,-1):
+            for y in range(img.shape[1]-1,start,-1):
                 if img[x, y, 0] == 8:
                     stop = y
-            if abs(start-stop)>maxh:
-                maxh = start-stop
+                    break
+            if abs(start-stop)>maxw:
+                maxw = abs(start-stop)
 
-        maxw = 0
+        maxh = 0
         for y in range((img.shape[1])):
             for x in range((img.shape[0])):
                 if img[x,y, 0] == 8:
                     start = x
                     break
-            for x in range(img.shape[0]-1,0,-1):
+            for x in range(img.shape[0]-1,start,-1):
                 if img[x, y, 0] == 8:
                     stop = x
-            if abs(start-stop)>maxw:
-                maxw = start-stop
+                    break
+            if abs(start-stop)>maxh:
+                maxh = abs(start-stop)
 
         return (maxw,maxh)
 
     areaPixel = np.count_nonzero(img == 8)
     maxw, maxh = hw(img)
-    actlSize = (0.004 * maxh) / depth
+
+    actlSize = ( (0.28 * maxh) / depth) # focal length, cm
     multiplier = actlSize/maxh
+
+    # print("maxw", maxw)
+    # print("maxh", maxh)
+    # print(multiplier, "multiplier")
+
     area = areaPixel*(multiplier**2)
-    maxwActl = maxw*maxh
+    maxwActl = maxw*multiplier
 
     return(area, maxwActl, actlSize)
+
+bitmapImg = cv2.imread("ntubitmap.jpg")
+area, maxwActl, heightActl = areas(bitmapImg, 111111*math.dist((1.3539504607263098,103.68779725423865)
+                                                             , (1.3539566667549052, 103.68758550537318))) # 111,111 is an approximation for lat and lng near the equator
+
+print("Area = ", area)
+print("width", maxwActl)
+print("height", heightActl)
